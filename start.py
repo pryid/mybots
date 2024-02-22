08f31a55cf1ddbcf983eb887a1b924298c2173a5
pryid <neyroesqualla@gmail.com>
2024-02-22 20:17:51 +0500

    starter fix

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def run_script(script_name):
-    subprocess.run(["python", script_name])
+    subprocess.Popen(["python", script_name])
 
 # Список скриптов для запуска
 scripts = ["tts_checker/main.py", "sberprimeplus/main.py", "sitrim/main.py", "musarskoy/main.py"]
