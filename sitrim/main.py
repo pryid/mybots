29bff00c51235e7fe59d358b56dae898b6c050a4
pryid <neyroesqualla@gmail.com>
2024-03-10 01:02:14 +0500

    updated to use url-cleaner lib

--- sitrim/main.py
+++ sitrim/main.py
@@ -1,50 +1,23 @@
 # Стандартные библиотеки Python
-import re
-from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
+import re, asyncio
 
 # Внешние библиотеки
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineQueryResultArticle, InputTextMessageContent
+from url_cleaner import UrlCleaner
 
 # Локальные импорты
 from constants import API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL_ID
 
-# Функция для чтения и парсинга файла с правилами
-def parse_rules():
-    with open('filter.txt', 'r') as file:
-        file_contents = file.read()
-
-    pattern = r'\|\|([^\^]+)\^\$removeparam=([^\n]+)'
-    rules = {}
-
-    for domain, params in re.findall(pattern, file_contents):
-        params = params.split('|')
-        params = list(set([param.strip() for param in params if param.strip()]))
-
-        if domain in rules:
-            rules[domain].extend(params)
-            rules[domain] = list(set(rules[domain]))
-        else:
-            rules[domain] = params
-
-    return rules
-
-rules = parse_rules()
+# Асинхронная функция для обновления правил
+async def update_rules_periodically():
+    while True:
+        UrlCleaner().ruler.update_rules()
+        await asyncio.sleep(3600)
 
 # Функция для удаления параметров из URL
-def remove_parameters_from_url(url, rules):
-    parsed_url = urlparse(url)
-    query_params = parse_qs(parsed_url.query)
-    domain = parsed_url.netloc
-
-    if domain in rules:
-        for param in rules[domain]:
-            query_params.pop(param, None)
-
-    new_query = urlencode(query_params, doseq=True)
-    new_url = urlunparse(parsed_url._replace(query=new_query))
-
-    return new_url
+def remove_parameters_from_url(url):
+    return UrlCleaner().clean(url)
 
 # Создание клиента Pyrogram
 app = Client("sitrim", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
@@ -54,14 +27,16 @@ def log_action(user, action):
     app.send_message(LOG_CHANNEL_ID, log_message)
 
 @app.on_message(filters.private & filters.command("start"))
-def start(client, message: Message):
+async def start(client, message: Message):
+    # Запуск асинхронной задачи обновления правил
+    asyncio.create_task(update_rules_periodically())
     message.reply_text("Hello. Send me a URL to clean it.")
     log_action(message.from_user, "Started the bot")
 
 @app.on_message(filters.private & filters.regex(r'https?://[^\s]+'))
 def clean_url(client, message: Message):
     url = message.text
-    cleaned_url = remove_parameters_from_url(url, rules)
+    cleaned_url = remove_parameters_from_url(url)
     message.reply_text(f"Cleaned URL:\n{cleaned_url}")
     log_action(message.from_user, f"Cleaned URL: {url} -> {cleaned_url}")
 
@@ -70,7 +45,7 @@ def answer(client, inline_query):
     try:
         query = inline_query.query
         if query.startswith("http"):
-            cleaned_url = remove_parameters_from_url(query, rules)
+            cleaned_url = remove_parameters_from_url(query)
             inline_query.answer([InlineQueryResultArticle(
                 title="Cleaned URL",
                 input_message_content=InputTextMessageContent(cleaned_url)
@@ -79,4 +54,5 @@ def answer(client, inline_query):
     except Exception as e:
         log_action(inline_query.from_user, f"Error in inline query: {e}")
 
+# Запуск бота
 app.run()
