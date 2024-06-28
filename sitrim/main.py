3ea783a26bc7a545b1172b06c0d96c50e1ad7781
pryid <neyroesqualla@gmail.com>
2024-06-29 02:59:36 +0500

    fix logging

--- sitrim/main.py
+++ sitrim/main.py
@@ -1,32 +1,40 @@
-# Стандартные библиотеки Python
-import re, asyncio
-
-# Внешние библиотеки
+import re
+import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineQueryResultArticle, InputTextMessageContent
 from url_cleaner import UrlCleaner
-
-# Локальные импорты
 from constants import API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL_ID
 
 # Асинхронная функция для обновления правил
 async def update_rules_periodically():
     while True:
-        UrlCleaner().ruler.update_rules()
+        try:
+            UrlCleaner().ruler.update_rules()
+        except Exception as e:
+            print(f"Error updating rules: {e}")
         await asyncio.sleep(36000)  # Спать 36000 секунд (10 часов)
 
 # Функция для удаления параметров из URL
 def remove_parameters_from_url(url):
-    return UrlCleaner().clean(url)
+    try:
+        return UrlCleaner().clean(url)
+    except Exception as e:
+        print(f"Error cleaning URL: {e}")
+        return url
 
 # Создание клиента Pyrogram
 app = Client("sitrim", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-asyncio.create_task(update_rules_periodically())  # Запуск асинхронной задачи обновления правил
+
+# Запуск асинхронной задачи обновления правил
+asyncio.create_task(update_rules_periodically())
 
 # Асинхронная функция для логирования
 async def log_action(user, action):
     log_message = f"@{user.username or ''}, {user.first_name or ''} {user.last_name or ''}, ID: {user.id} - {action}"
-    await app.send_message(LOG_CHANNEL_ID, log_message)
+    try:
+        await app.send_message(LOG_CHANNEL_ID, log_message)
+    except Exception as e:
+        print(f"Error logging action: {e}")
 
 @app.on_message(filters.private & filters.command("start"))
 async def start(client, message: Message):
@@ -55,4 +63,5 @@ async def answer(client, inline_query):
         await log_action(inline_query.from_user, f"Error in inline query: {e}")
 
 # Запуск бота
-app.run()
+if __name__ == "__main__":
+    app.run()
