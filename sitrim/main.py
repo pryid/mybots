f18814a2a32dd9cf1a16354828b4c4a229d09ac7
pryid <neyroesqualla@gmail.com>
2024-07-01 00:48:17 +0500

    apscheduler fix

--- sitrim/main.py
+++ sitrim/main.py
@@ -1,29 +1,27 @@
-import asyncio
-import logging
+# Стандартные библиотеки Python
+import re, asyncio, logging
+
+# Внешние библиотеки
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineQueryResultArticle, InputTextMessageContent
 from url_cleaner import UrlCleaner
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+# Локальные импорты
 from constants import API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL_ID
 
-# Настройка уровня логирования для Pyrogram
 logging.getLogger('pyrogram').setLevel(logging.WARNING)
+logging.getLogger('url_cleaner').setLevel(logging.WARNING)
+logging.getLogger('apscheduler').setLevel(logging.WARNING)
 
 # Асинхронная функция для обновления правил
 async def update_rules_periodically():
     while True:
-        try:
-            UrlCleaner().ruler.update_rules()
-        except Exception as e:
-            await log_error(f"Error updating rules: {e}")
-        await asyncio.sleep(36000)  # Спать 36000 секунд (10 часов)
+        UrlCleaner().ruler.update_rules()
 
 # Функция для удаления параметров из URL
 def remove_parameters_from_url(url):
-    try:
-        return UrlCleaner().clean(url)
-    except Exception as e:
-        asyncio.create_task(log_error(f"Error cleaning URL: {e}"))
-        return url
+    return UrlCleaner().clean(url)
 
 # Создание клиента Pyrogram
 app = Client("sitrim", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
@@ -31,20 +29,11 @@ app = Client("sitrim", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 # Асинхронная функция для логирования
 async def log_action(user, action):
     log_message = f"@{user.username or ''}, {user.first_name or ''} {user.last_name or ''}, ID: {user.id} - {action}"
-    try:
-        await app.send_message(LOG_CHANNEL_ID, log_message)
-    except Exception as e:
-        await log_error(f"Error logging action: {e}")
-
-async def log_error(error_message):
-    try:
-        await app.send_message(LOG_CHANNEL_ID, error_message)
-    except Exception as e:
-        # В случае ошибки логирования в канал, ничего не делаем
-        pass
+    await app.send_message(LOG_CHANNEL_ID, log_message)
 
 @app.on_message(filters.private & filters.command("start"))
 async def start(client, message: Message):
+    asyncio.create_task(update_rules_periodically())  # Запуск асинхронной задачи обновления правил
     await message.reply_text("Hello. Send me a URL to clean it.")
     await log_action(message.from_user, "Started the bot")
 
@@ -67,12 +56,12 @@ async def answer(client, inline_query):
             )])
             await log_action(inline_query.from_user, f"Cleaned inline URL: {query} -> {cleaned_url}")
     except Exception as e:
-        await log_error(f"Error in inline query: {e}")
+        await log_action(inline_query.from_user, f"Error in inline query: {e}")
 
 # Запуск бота
-if __name__ == "__main__":
-    try:
-        asyncio.create_task(update_rules_periodically())  # Запуск периодической задачи
-        app.run()
-    except Exception as e:
-        print(f"Error starting bot: {e}")
+
+scheduler = AsyncIOScheduler()
+scheduler.add_job(update_rules_periodically, "interval", seconds=36000)
+scheduler.start()
+
+app.run()
