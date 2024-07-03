6e183e124da0e30bd065d5c95177403041723b09
pryid <neyroesqualla@gmail.com>
2024-07-03 05:44:42 +0500

    save photo_ids instead of photos, typing and upload statuses

--- musarskoy/main.py
+++ musarskoy/main.py
@@ -13,7 +13,7 @@ logging.getLogger('pyrogram').setLevel(logging.WARNING)
 musarskoy_id = 1473899765
 admin_id = 768483882
 responses_file = '/app/data/musarskoy/responses.json'
-photo_folder = '/app/data/musarskoy/photo'
+photos_file = '/app/data/musarskoy/photos.json'
 
 # Загрузка JSON-файла с ответами
 try:
@@ -26,6 +26,17 @@ except Exception as e:
         await client.send_message(LOG_CHANNEL_ID, f"Error loading responses: {e}")
     responses = []
 
+# Загрузка JSON-файла с photo_ids
+try:
+    with open(photos_file, "r") as file:
+        data = json.load(file)
+        photo_ids = data.get("photo_ids", [])
+except Exception as e:
+    # Логирование ошибки в канал
+    async def log_error_to_channel(client, message):
+        await client.send_message(LOG_CHANNEL_ID, f"Error loading photo_ids: {e}")
+    photo_ids = []
+
 # Функция для обновления и перезагрузки ответов
 def update_and_reload_responses(new_response):
     responses.append(new_response)
@@ -37,37 +48,46 @@ def update_and_reload_responses(new_response):
         async def log_error_to_channel(client, message):
             await client.send_message(LOG_CHANNEL_ID, f"Error saving responses: {e}")
 
-# Функция для отправки рандомных фото из папки
-def send_random_photo():
+# Функция для обновления и перезагрузки photo_ids
+def update_and_reload_photo_ids(new_photo_id):
+    photo_ids.append(new_photo_id)
     try:
-        photos = os.listdir(photo_folder)
-        if photos:
-            photo = choice(photos)
-            return os.path.join(photo_folder, photo)
+        with open(photos_file, "w") as file:
+            json.dump({"photo_ids": photo_ids}, file, ensure_ascii=False, indent=4)
+    except Exception as e:
+        # Логирование ошибки в канал
+        async def log_error_to_channel(client, message):
+            await client.send_message(LOG_CHANNEL_ID, f"Error saving photo_ids: {e}")
+
+# Функция для отправки рандомных фото из списка photo_ids
+def send_random_photo_id():
+    try:
+        if photo_ids:
+            photo_id = choice(photo_ids)
+            return photo_id
         else:
             return None
     except Exception as e:
         # Логирование ошибки в канал
         async def log_error_to_channel(client, message):
-            await client.send_message(LOG_CHANNEL_ID, f"Error listing photos: {e}")
+            await client.send_message(LOG_CHANNEL_ID, f"Error selecting random photo_id: {e}")
         return None
 
 # Создание клиента Pyrogram
 app = Client("musarskoy", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-# Функция для сохранения фотографий пациента в папку
+# Функция для сохранения file_id фотографий пациента
 @app.on_message(filters.photo & (filters.user(musarskoy_id) | filters.user(admin_id)))
 async def save_photo_from_user(client, message: Message):
     try:
         photo = message.photo
         file_id = photo.file_id
-        save_path = os.path.join(photo_folder, f"{file_id}.jpg")
-        await client.download_media(message, save_path)
-        # Логирование успешного сохранения фото в канал
-        await client.send_message(LOG_CHANNEL_ID, f"Photo saved to {save_path}")
+        update_and_reload_photo_ids(file_id)
+        # Логирование успешного сохранения photo_id в канал
+        await client.send_message(LOG_CHANNEL_ID, f"Photo ID saved: {file_id}")
     except Exception as e:
         # Логирование ошибки в канал
-        await client.send_message(LOG_CHANNEL_ID, f"Error saving photo: {e}")
+        await client.send_message(LOG_CHANNEL_ID, f"Error saving photo ID: {e}")
 
 # Функции для проверки наличия ключевых слов в сообщении
 def check_message_for_keywords(message_text):
@@ -92,7 +112,6 @@ async def echo(client, message):
     # Проверка ID пользователя
     if message.from_user.id == musarskoy_id:
         update_and_reload_responses(message.text)
-        #await message.reply("Ваше сообщение добавлено в список ответов.")
         await client.send_message(LOG_CHANNEL_ID, f"New response added: {message.text}")
     else:
         # Генерация случайного числа от 1 до 100
@@ -100,35 +119,40 @@ async def echo(client, message):
 
         # Проверка случайного числа для отправки фото
         if random_number == 2:
-            photo_path = send_random_photo()
-            if photo_path:
-                await message.reply_photo(photo_path)
-                await client.send_message(LOG_CHANNEL_ID, f"Sent random photo: {photo_path}")
+            photo_id = send_random_photo_id()
+            if photo_id:
+                await message.reply_chat_action("upload_photo")
+                await message.reply_photo(photo_id)
+                await client.send_message(LOG_CHANNEL_ID, f"Sent random photo: {photo_id}")
             else:
                 await message.reply("Фото отсутствуют.")
-                await client.send_message(LOG_CHANNEL_ID, "No photos available to send.")
+                await client.send_message(LOG_CHANNEL_ID, "No photo IDs available to send.")
         # Проверка случайного числа для ответа без учета ключевых слов
         elif random_number == 1:
             response = choice(responses)
+            await message.reply_chat_action("typing")
             await message.reply(response)
             await client.send_message(LOG_CHANNEL_ID, f"Sent random response: {response}")
         # Проверка для ответа с учетом ключевых слов    
         elif check_message_for_keywords(message.text):
             response = choice(responses)
+            await message.reply_chat_action("typing")
             await message.reply(response)
             await client.send_message(LOG_CHANNEL_ID, f"Sent keyword-based response: {response}")
         # Проверка для ответа фото с учетом ключевых слов    
         elif check_message_for_keywords_photo(message.text):
-            photo_path = send_random_photo()
-            if photo_path:
-                await message.reply_photo(photo_path)
-                await client.send_message(LOG_CHANNEL_ID, f"Sent keyword-based photo: {photo_path}")
+            photo_id = send_random_photo_id()
+            if photo_id:
+                await message.reply_chat_action("upload_photo")
+                await message.reply_photo(photo_id)
+                await client.send_message(LOG_CHANNEL_ID, f"Sent keyword-based photo: {photo_id}")
             else:
                 await message.reply("Фото отсутствуют.")
-                await client.send_message(LOG_CHANNEL_ID, "No photos available to send for keyword-based response.")
+                await client.send_message(LOG_CHANNEL_ID, "No photo IDs available to send for keyword-based response.")
         elif message.reply_to_message and message.reply_to_message.from_user.id == client.me.id:
             # Если сообщение является ответом на сообщение бота
             response = choice(responses)
+            await message.reply_chat_action("typing")
             await message.reply(response)
             await client.send_message(LOG_CHANNEL_ID, f"Replied to bot's message: {response}")
 
